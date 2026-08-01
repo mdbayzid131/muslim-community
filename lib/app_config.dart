90c69105e411e3390a50e24eb87d21e43429d1c8
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-08-01 11:59:46 +0600

    feat: add AppConfig class for API endpoints and increment app version to 1.0.2+3

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String baseUrl = "https://adnan5001.binarybards.online/api/v1";
+  static const String baseUrl = "https://nayem5001.binarybards.online/api/v1";
   static const String createUserEndpoint = "$baseUrl/users";
   static const String verifyOtpEndpoint = "$baseUrl/auth/verify-otp";
   static const String loginEndpoint = "$baseUrl/auth/login";
